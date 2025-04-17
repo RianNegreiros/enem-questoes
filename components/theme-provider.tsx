597dd3eb20eae7ff0e38d98d68025b2b7bf06fb7
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-04-17 11:10:32 -0300

    Initial commit

---
+++ components/theme-provider.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import * as React from 'react'
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps,
+} from 'next-themes'
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const [mounted, setMounted] = React.useState(false)
+
+  // useEffect only runs on the client, so we can safely show the UI once it's mounted
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return (
+    <NextThemesProvider {...props}>
+      {mounted ? children : <div style={{ visibility: 'hidden' }}>{children}</div>}
+    </NextThemesProvider>
+  )
+}
